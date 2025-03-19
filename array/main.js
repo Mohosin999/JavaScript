13b3e57aeb9c42d880af9cc99d396b12f9b67509
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-03-19 09:17:07 +0600

    push method completed

--- array/main.js
+++ array/main.js
@@ -1,7 +1,22 @@
 /**
- * Push method
+ * push() method
  */
 
-const arr = [1, 2];
-arr.push(3)
-console.log(arr);
+const arr1 = [1, 2];
+arr1.push(3);
+
+const pushArr = (array, ...elements) => {
+  for (const element of elements) {
+    array[array.length] = element;
+  }
+
+  return array.length;
+};
+
+console.log(pushArr(arr1, 4, 5, 6));
+console.log(arr1);
+
+/**
+ * pop() method
+ */
+const arr2 = [1, 2, 3, 4, 5];
