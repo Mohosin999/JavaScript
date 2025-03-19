a0062f6dcaab582ed38fcc931fcd3b47dd63c821
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-03-19 09:39:05 +0600

    pop() method

--- array/main.js
+++ array/main.js
@@ -1,22 +1,38 @@
+const arr = [1, 2, 3, 4, 5];
+
 /**
  * push() method
  */
 
-const arr1 = [1, 2];
-arr1.push(3);
+// const arr1 = [1, 2];
+// arr1.push(3);
 
-const pushArr = (array, ...elements) => {
-  for (const element of elements) {
-    array[array.length] = element;
-  }
+// const pushArr = (array, ...elements) => {
+//   for (const element of elements) {
+//     array[array.length] = element;
+//   }
 
-  return array.length;
-};
+//   return array.length;
+// };
 
-console.log(pushArr(arr1, 4, 5, 6));
-console.log(arr1);
+// console.log(pushArr(arr1, 4, 5, 6));
+// console.log(arr1);
 
 /**
  * pop() method
  */
-const arr2 = [1, 2, 3, 4, 5];
+// const arr = [1, 2, 3, 4, 5];
+// // arr2.pop();
+
+// const popArr = (array) => {
+//   if (array.length === 0) return undefined;
+//   const lastElement = array[array.length - 1];
+
+//   array.length = arr.length - 1;
+//   return lastElement;
+// };
+
+// popArr(arr);
+// popArr(arr);
+// popArr(arr);
+// console.log(arr); // [1, 2]
